fc98aa022f7a043323ccf000af021bfba7f61a47
Shinu <spushpan@expediagroup.com>
2020-03-26 17:49:12 -0700

    Added header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import { Route, Switch} from 'react-router-dom';
+import Header from './components/header/header.component'
 
 const HatsPage = () =>(
   <div>
@@ -13,6 +14,7 @@ const HatsPage = () =>(
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
       <Route exact component={HomePage} path='/' />    
       <Route component={ShopPage} path='/shop' />   
