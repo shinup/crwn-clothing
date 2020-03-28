7dc0ef32d63eb85f4e0731b5cff244cfcd630705
Shinu <spushpan@expediagroup.com>
2020-03-27 19:04:43 -0700

    Finished login

--- src/App.js
+++ src/App.js
@@ -30,14 +30,13 @@ class  App extends React.Component {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(snapShot =>{
-          this.setState({
-            currentUser :{
-            id:snapShot.id,
-            ...snapShot.data()
-          }
-        });
-        console.log(this.state);
+          userRef.onSnapshot(snapShot =>{
+            this.setState({
+              currentUser :{
+              id:snapShot.id,
+              ...snapShot.data()
+            }
+          });       
         });
         
       }
