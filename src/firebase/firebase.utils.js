cad0b67e139684bad79f4411ec93f7f7b2a7e1e8
Shinu <spushpan@expediagroup.com>
2020-03-27 18:56:25 -0700

    Added signup

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,37 @@ const config = {
     measurementId: "G-X376XZFD8W"
   };
 
+  export const createUserProfileDocument = async(userAuth, additionalData) =>{
+      if(!userAuth){
+          return;
+      }
+      const userRef = firestore.doc(`users/${userAuth.uid}`);
+      const snapShot = await userRef.get();
+      
+      if(!snapShot.exists){
+          const { displayName, email} = userAuth;
+          const createdAt = new Date();
+
+          try {
+
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+
+            })
+              
+          } catch (error) {
+              console.log('erro creating user', error.message);
+          }
+
+      }
+
+      return userRef;
+  }
+
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
