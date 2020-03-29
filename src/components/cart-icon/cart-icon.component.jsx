1bfc13bcec8ba1027eb87dc22675d1a46dbd0bee
Shinu <spushpan@expediagroup.com>
2020-03-28 21:56:48 -0700

    Updated applciation with reslect replaced cart selctors to improve perfomance

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import './cart-icon.styles.scss';
-import  {ReactComponent as ShoppingIcon} from '../../assets/cart.svg';
 import { connect } from 'react-redux';
-import {toggleCartHidden} from '../../redux/cart/cart.action';
 
-const CartIcon = ({toggleCartHidden}) =>(
+import  {ReactComponent as ShoppingIcon} from '../../assets/cart.svg';
+import {toggleCartHidden} from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
+import './cart-icon.styles.scss';
+
+const CartIcon = ({toggleCartHidden, itemCount}) =>(
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 );
 
@@ -15,7 +18,12 @@ const mapDispatchToProps = dispatch =>({
     toggleCartHidden : () =>  dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = (state) =>({
+    itemCount : selectCartItemsCount(state)
+
+});
+
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 ) (CartIcon);
